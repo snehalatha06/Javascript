5184206716be436ef5b6ba0a0fe30bdaf098b650
snehalatha06 <111651894+snehalatha06@users.noreply.github.com>
2022-08-25 15:53:53 +0530

    Add files via upload

---
+++ ATM.js
@@ -0,0 +1,52 @@
+console.log('welcome ATM');
+pin=prompt('enter a your pin');
+balance=40000;
+pin==1532;
+deposite=5000;
+withdraw=4000;
+console.log('1.deposite,2.balance,3.withdraw,4.mini-staement');
+option=prompt('enter no');
+  if (option==1){
+    deposite=5000;
+    balance=balance+deposite;
+    console.log('total balance');
+  } else if(option==2){
+    console.log('balance');
+  }else if(option==3){
+    balance=withdraw-balance
+    console.log('balance');
+  }
+  else if(option==4){
+    console.log('----atm');
+    console.log('username');
+    console.log('totalbalace');
+    console.log('thank you');
+  }
+else{
+  console.log('enter correct password')
+}
+var day=prompt('enter a day')
+if (day=='monday'){
+  console.log('upma');
+}
+else if(day=='tuesday'){
+  console.log('pasta');
+}
+else if(day=='wednesday'){
+  console.log('sprouts');
+}
+else if(day=='thusday'){
+  console.log('maggie');
+} 
+else if(day=='friday'){
+  console.log('vermiselly');
+}
+const name = 'Peter';
+const name1 = "Jack";
+const result = `The names are ${name} and ${name1}`;
+console.log(name1);
+let a = 'hello';
+a = 'Hello';
+console.log(a); 
+
+               
